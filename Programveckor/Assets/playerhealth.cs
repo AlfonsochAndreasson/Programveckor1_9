699a38edde577ea4e6ce0b47e4f784e6ccb416d8
friendlydew <nathalie.modin@elev.ga.lbs.se>
2025-01-14 15:08:36 +0100

    scene

--- Programveckor/Assets/playerhealth.cs
+++ Programveckor/Assets/playerhealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerhealth : MonoBehaviour
 {
@@ -21,7 +22,7 @@ public class playerhealth : MonoBehaviour
         health -= amount;
         if(health <= 0)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(2);
         }
     }
 
