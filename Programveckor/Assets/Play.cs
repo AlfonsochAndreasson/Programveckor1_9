fa337ee1346d9fb25bdc498f2113e0d2f05a6dc5
friendlydew <nathalie.modin@elev.ga.lbs.se>
2025-01-15 13:15:10 +0100

    Gamestart

---
+++ Programveckor/Assets/Play.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public void GoToSceneOne()
+    {
+        SceneManager.LoadScene("die");
+
+    }
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
